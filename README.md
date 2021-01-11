6c29520dd6790f760037332573a0efca297c2575
pree-271124 <74281559+pree-271124@users.noreply.github.com>
2021-01-11 01:37:23 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# maggi
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,6 @@
+1. Take a maggi packet .
+2. Take some water . 
+3. Take a pan .
+4. Pour water and spices into the pan . 
+5. Heat the pan and make the water boil . 
+6. Cook the maggi for 2 minutes .
